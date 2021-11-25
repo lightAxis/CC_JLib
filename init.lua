9754aecbfeee367bbcdeca12bd63629dfbbef4f0
jisuk500 <jisuk500@gmail.com>
2021-11-26 02:39:22 +0900

    errata fix

--- init.lua
+++ init.lua
@@ -1,3 +1,3 @@
 -- add require package path of jasuk500 custom library
 -- os.loadAPI("package")
-package.path = package.path..";CC_Lib/JLib/?.lua"
+package.path = package.path..";CC_JLib/JLib/?.lua"
