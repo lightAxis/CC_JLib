fe1f974f74a4b02ef370b01b0edcd2b57403990b
jisuk500 <jisuk500@gmail.com>
2021-12-28 01:13:52 +0900

    add bank proj init

---
+++ BankProj/main.lua
@@ -0,0 +1,45 @@
+--- includes basic components
+require("UI.Includes")
+require("EventRouter.Includes")
+
+--- make project global namespace
+---@class BankProj
+local BankProj = {}
+
+--- initialize EventRouter
+--- this routes to UI Event and rednet event to other functions
+BankProj.EventRouter = JLib.EventRouter.Router:new()
+
+--- initialize UIRunner
+--- this runs Scene UI intercation system from EventRouter
+BankProj.UIRunner = JLib.UIRunner:new()
+--- attach UIRunner to EventRouter
+BankProj.EventRouter:attachUIRunner(BankProj.UIRunner)
+
+--- initialize Screen objects to use,
+--- can terminal or monitor
+--- side must exist. NONE is terminal
+local screen_term = JLib.Screen:new(term, JLib.Enums.Side.NONE)
+
+------------ SCENENS -----------
+
+--- register Project SceneFile to other 
+local MainScene = require("BankProj.MainScene")
+
+--- register Project Scene Instance to Project global namespace
+BankProj.MainScene = MainScene:new(BankProj, screen_term)
+
+--- register each screen sides initialize Scene
+BankProj.UIRunner:attachScene(BankProj.MainScene)
+
+--- set initial scene to start interact
+BankProj.UIRunner:setIntialScene(JLib.Enums.Side.NONE)
+
+-----------------------------------
+
+--- clear and readner initial scenes
+BankProj.UIRunner:ClearScreens()
+BankProj.UIRunner:RenderScreen()
+
+--- run main EventLoop to start Proj
+BankProj.EventRouter:main()
