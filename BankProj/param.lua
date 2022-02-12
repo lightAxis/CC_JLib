452433efdd0ed639a0bd8b7c58f6542d25714b88
jisuk500 <jisuk500@gmail.com>
2022-02-13 00:43:23 +0900

    fix bank my computer id parameter to automatically find id

--- BankProj/param.lua
+++ BankProj/param.lua
@@ -3,6 +3,6 @@ BankProj = BankProj or {}
 BankProj.Param = {
     ["PlayerDetectorName"] = "playerDetector",
     ["BankServerID"] = 17,
-    ["myID"] = 8,
+    ["myID"] = os.getComputerID(),
     ["rednet_side"] = "right"
 }
